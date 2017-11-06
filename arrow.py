0024bd6248f07ceb1932d8deda5a495ad8b6c042
lloydouyang <do5xb@virginia.edu>
2017-11-05 22:42:32 -0500

    done. report to do

--- arrow.py
+++ arrow.py
@@ -1,11 +1,13 @@
-target = [[0 for x in range(7)] for y in range(7)]
+t = [[0 for x in range(7)] for y in range(7)]
+pi = [[[0 for z in range(2)]for x in range(7)] for y in range(7)]
 discountfat = 1
-MAX_ITERS = 1000
+MAX_ITERS = 10
 epsilon = 0.001
 dirs = ["N", "S", "E", "W", "NE", "NW", "SE", "SW", "STAY"]
+displacement=[[-1,0],[1,0],[0,1],[0,-1],[-1,1],[-1,-1],[1,1],[1,-1],[0,0]]
 windcase = 0
-def printmatrix():
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in target]))
+def printmatrix(mm):
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in mm]))
 
 # Reward function, returns 0 if reaches point (3,6) else return -1
 def reward(direction):
@@ -16,87 +18,67 @@ def reward(direction):
 
 def wind(direction):
     if direction[1] >=3 and direction[1] <=5:
-        direction = (direction[0]-windcase, direction[1])
+        direction = [direction[0]-windcase, direction[1]]
     if direction[0] < 0:
-        direction = (0, direction[1])
+        direction = [0, direction[1]]
     return direction
 
-def move(curloc,dir):
+def move(curloc,k):
+    curloc[0]= curloc[0] + displacement[k][0]
+    curloc[1]= curloc[1] + displacement[k][1]
+    if curloc[0]<0: curloc[0]=0
+    if curloc[1]<0: curloc[1]=0
+    if curloc[0]>6: curloc[0]=6
+    if curloc[1]>6: curloc[1]=6
     curloc = wind(curloc)
-    if dir == "N":
-        if curloc[0]-1 >= 0:
-            curloc = (curloc[0]-1, curloc[1])
-    elif dir == "S":
-        if curloc[0]+1 <= 6:
-            curloc = (curloc[0]+1, curloc[1])
-    elif dir == "E":
-        if curloc[1]-1 >= 0:
-            curloc = (curloc[0], curloc[1]-1)
-    elif dir == "S":
-        if curloc[1]+1 <= 6:
-            curloc = (curloc[0], curloc[1]+1)
-    elif dir == "NE":
-        if curloc[0]-1 <= 0 and curloc[1]-1 <= 0:
-            return curloc
-        elif curloc[0]-1 <= 0:
-            curloc = (curloc[0], curloc[1]-1)
-        elif curloc[1]-1 <= 0:
-            curloc = (curloc[0]-1, curloc[1])
-        else:
-            curloc = (curloc[0]-1, curloc[1]-1)
-    elif dir == "NW":
-        if curloc[0]-1 <= 0 and curloc[1]+1 >= 6:
-            return curloc
-        elif curloc[0]-1 <= 0:
-            curloc = (curloc[0], curloc[1]+1)
-        elif curloc[1]+1 >= 6:
-            curloc = (curloc[0]-1, curloc[1])
-        else:
-            curloc = (curloc[0]-1, curloc[1]+1)
-    elif dir == "SE":
-        if curloc[0]+1 >= 6 and curloc[1]-1 <= 0:
-            return curloc
-        elif curloc[0]+1 <= 6:
-            curloc = (curloc[0], curloc[1]-1)
-        elif curloc[1]-1 <= 0:
-            curloc = (curloc[0]+1, curloc[1])
-        else:
-            curloc = (curloc[0]+1, curloc[1]-1)
-    elif dir == "SW":
-        if curloc[0]+1 >= 6 and curloc[1]+1 >= 6:
-            return curloc
-        elif curloc[0]+1 >= 6:
-            curloc = (curloc[0], curloc[1]+1)
-        elif curloc[1]+1 >= 6:
-            curloc = (curloc[0]+1, curloc[1])
-        else:
-            curloc = (curloc[0]+1, curloc[1]+1)
-    elif dir == "STAY":
-        curloc = curloc
     return curloc
 
-def val_iter(target):
+def val_iter():
     iters = 0
-    reference = target
+    target = [[-1 for x in range(7)] for y in range(7)]
+    target[3][6]=0
+    reference = [[0 for x in range(7)] for y in range(7)]
     while iters < MAX_ITERS:
         iters += 1
         delta = 0
         direction = "STAY"
-        rewards = -100000000
+       
         for i in range(7):
             for j in range(7):
-                pos = (i,j)
-                for k in dirs:
-                    next_step = move(pos,k)
+                reference[i][j]=target[i][j]
+        for i in range(7):
+            for j in range(7):
+                rewards = -100000000
+                for k in range(9):
+                    next_step = move([i,j],k)
                     re_sum = reference[next_step[0]][next_step[1]]
                     if re_sum > rewards:
-                        reward = re_sum
-                        direction = k
-                target[i][j] = reward(pos) + epsilon*reward
-                if target[i][j] - reference[i][j] > delta:
-                    delta = target[i][j] - reference[i][j]
+                        rewards = re_sum
+                        pi[i][j][0] = k
+                        pi[i][j][1] = 0
+                    else:
+                        if re_sum == rewards:
+                            pi[i][j][1] =k
+                     
+                target[i][j] = reward([i,j]) + rewards
+                if abs(target[i][j] - reference[i][j]) > delta:
+                    delta = abs(target[i][j] - reference[i][j])
         if delta < epsilon:
             break
-    return reference
+    return target
             
-printmatrix()
+#printmatrix(t)
+printmatrix(val_iter())
+t1 = [[0 for x in range(7)] for y in range(7)]
+for i in range(7):
+    for j in range(7):
+        t1[i][j]=pi[i][j][0]
+printmatrix(t1)
+for i in range(7):
+    for j in range(7):
+        t1[i][j]=pi[i][j][1]
+printmatrix(t1)
+
+
+
+
